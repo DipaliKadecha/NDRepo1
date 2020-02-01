edca8702a8b68326149f2d8460202f90dedbb072
DipaliKadecha <dipalikadecha92@gmail.com>
2020-02-01 20:11:20 +0530

    Initial first commit of project.

---
+++ NDShare/src/com/nd/application/Shopping.java
@@ -0,0 +1,10 @@
+package com.nd.application;
+
+public class Shopping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Weekend stuff");
+	}
+
+}
